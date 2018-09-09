860b67e5fdbb86d0626d6ab3a9e55b64ee522d17
Bertrik Sikken <bertrik@sikken.nl>
2018-09-09 23:10:26 +0200

    Initial commit (untested).

---
+++ esprfid.ino
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiManager.h>
+#include <PubSubClient.h>
+
+#include <MFRC522.h>
+
+// TODO give these a meaningful value
+#define PIN_MFRC_SS  3
+#define PIN_MFRC_RST 4
+
+static WiFiManager wifiManager;
+static WiFiClient wifiClient;
+static MFRC522 mfrc522(PIN_MFRC_SS, PIN_MFRC_RST);
+
+void setup(void)
+{
+    // initialize serial port
+    Serial.begin(115200);
+    Serial.println("RFID reader\n");
+
+    // initialise card reader
+    SPI.begin();
+    mfrc522.PCD_Init();
+
+    // connect to wifi
+    Serial.println("Starting WIFI manager ...");
+    wifiManager.autoConnect("ESP-RFID");
+}
+
+static int post_uid(int len, const unsigned char *uid)
+{
+    // create UID string
+    char payload[128];
+    strcpy(payload, "");
+    for (int i = 0; i < len; i++) {
+        char buf[8];
+        sprintf(buf, "%02X", uid[i]);
+        strcat(payload, buf);
+    }
+    
+    // send it over HTTP
+    HTTPClient http;
+    http.begin("http://server.com/api");
+    int result = http.POST((uint8_t *)payload, strlen(payload));
+    http.end();
+    
+    return result;
+}
+
+void loop(void)
+{
+    if (mfrc522.PICC_IsNewCardPresent()) {
+        if (mfrc522.PICC_ReadCardSerial()) {
+            Serial.println("Detected card!");
+            // send the UID
+            int result = post_uid(4, mfrc522.uid.uidByte);
+            Serial.print("Result: ");
+            Serial.println(result, DEC);
+        }
+    }
+}
+
